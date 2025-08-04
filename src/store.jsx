f114ab01b4da9f2857e130733f277f493c4811cb
ashish23072002 <ashish23bisht@gmail.com>
2025-08-04 22:39:41 +0530

    Create Reducer Functions

---
+++ src/store.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-case-declarations */
+//this is the Reducer function
+const ADD_Task = "task/add";
+const DELETE_Task = "task/delete";
+
+const intialState = {
+  task: [],
+};
+const taskReducer = (state = intialState, action) => {
+  switch (action.type) {
+    case ADD_Task:
+      return {
+        ...state,
+        task: [...state.task, action.payload],
+      };
+    case DELETE_Task:
+      const updatedTask = state.task.filter((curTask, index) => {
+        return index !== action.payload;
+      });
+      return {
+        ...state,
+        task: [...state.task, updatedTask],
+      };
+
+    default:
+      return state;
+  }
+};
