bbe4d964c7da93f5602e7091618a9901bc134f3f
ashish23072002 <ashish23bisht@gmail.com>
2025-08-12 21:50:04 +0530

    Formated file

--- src/store.jsx
+++ src/store.jsx
@@ -4,7 +4,7 @@ import { thunk } from "redux-thunk";
 
 /* eslint-disable no-case-declarations */
 
-//this is the Reducer function (Create Reducer Functions )
+// Reducer action types
 const ADD_Task = "task/add";
 const DELETE_Task = "task/delete";
 const FEATCH_TASK = "task/featch";
@@ -13,7 +13,7 @@ const intialState = {
   task: [],
 };
 
-// create a simple reducer function
+// Create a simple reducer function
 const taskReducer = (state = intialState, action) => {
   switch (action.type) {
     case ADD_Task:
@@ -40,37 +40,34 @@ const taskReducer = (state = intialState, action) => {
   }
 };
 
-// create react store using reducer
-// installed redux
-
-//Step 2: Crete  the redux store using the redcer
+// Create Redux store using reducer and middleware
 export const store = createStore(
   taskReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
 console.log(store);
-// login to the intialState
+// Log the initial state
 console.log("Initial state", store.getState());
 
-// Step 4: DisPatch an action to Add a Task
-//how to Dispatch a method Using Dispatch Method (for adding into the array)
+// Dispatch an action to add a task
+// Using dispatch method to add into the array
 store.dispatch({ type: ADD_Task, payload: "Add this line" });
 console.log("Updated state", store.getState());
 
-//(for adding into the array)
+// Add another task
 store.dispatch({
   type: ADD_Task,
   payload: "This is the second Line for adding",
 });
 console.log("Updated state", store.getState());
 
-//deleteing
+// Deleting a task
 store.dispatch({ type: DELETE_Task, payload: 1 });
 console.log("Deleted state", store.getState());
 
-//recreating the 47 line where we are dispatching the task Using(Action Creator)
-// Step 5: creating action creator
-
+// Dispatching tasks using action creators
+// Step: Creating action creators
 export const addTask = (data) => {
   return { type: ADD_Task, payload: data };
 };
@@ -79,16 +76,15 @@ export const deleteTask = (id) => {
   return { type: DELETE_Task, payload: id };
 };
 
-store.dispatch(addTask("This line is added by Action Creator or a fucntion "));
-store.dispatch(addTask("Buy apple  banana and  kiwiw "));
-store.dispatch(addTask("Buy Tamato "));
+store.dispatch(addTask("This line is added by Action Creator or a function"));
+store.dispatch(addTask("Buy apple banana and kiwi"));
+store.dispatch(addTask("Buy Tomato"));
 console.log(store.getState());
 
 store.dispatch(deleteTask(1));
 console.log(store.getState());
 
-//creating a middleware with the help of Redux Thunk
-
+// Creating a middleware with Redux Thunk to fetch tasks
 export const featchTask = () => {
   return async (dispatch) => {
     try {
@@ -96,8 +92,11 @@ export const featchTask = () => {
         "https://jsonplaceholder.typicode.com/todos?_limit=3"
       );
       const task = await res.json();
-      dispatch({ type: FEATCH_TASK, payload: task.map((curTask)=>curTask.title) });
-      console.log(task)
+      dispatch({
+        type: FEATCH_TASK,
+        payload: task.map((curTask) => curTask.title),
+      });
+      console.log(task);
     } catch (error) {
       console.log(error);
     }
